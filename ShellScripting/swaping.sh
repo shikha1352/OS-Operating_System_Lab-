af8dfd9faa7e115c2e11cbb7cccab7e240756c4c
shikha1352 <pnsshikha@gmail.com>
2025-06-10 09:54:12 -0300

    swaping

---
+++ ShellScripting/swaping.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# shell program to swap to values
+echo -n "Enter the value of A: "
+read a
+echo -n "Enter the value of B: "
+read b
+t=$a 
+a=$b 
+b=$t
+echo "values after swapping"
+echo "A value is $a"
+echo "B value is $b"
