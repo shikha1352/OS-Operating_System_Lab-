9cf9a8ad41f264fbaedb7f820261c5c30dbd9722
shikha1352 <pnsshikha@gmail.com>
2025-06-17 12:33:59 -0300

    RoundRobin

--- CPUScheduling/RoundRobin.cpp
+++ CPUScheduling/RoundRobin.cpp
@@ -1,78 +1,97 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int n, tq;
+    int n;
     cout << "Enter number of processes: ";
     cin >> n;
-    vector<int> pid(n), bt(n), remaining_bt(n), wt(n, 0), tat(n), ct(n);
-    cout << "Enter Burst Time for each process:\n";
-    for (int i = 0; i < n; i++) {
-        pid[i] = i + 1;
-        cin >> bt[i];
-        remaining_bt[i] = bt[i];
+    vector<int>p(n), At(n), bt(n), ct(n), tat(n), wt(n), rb(n);
+    cout << "Enter arrival time and burst time:\n";
+    for (int i = 0; i < n; ++i) {
+            p[i]=i+1;
+        cin >> At[i] >> bt[i];
+        rb[i] = bt[i];
     }
-
-    cout << "Enter Time Quantum: ";
-    cin >> tq;
-
+    int qt;
+    cout << "Enter quantum time: ";
+    cin >> qt;
     queue<int> q;
-    for (int i = 0; i < n; i++) {
-        q.push(i);
-    }
-    int time = 0;
-    vector<int> gantt;
-    while (!q.empty()) {
+    vector<bool> visited(n, false);
+    vector<int>gp,gc;
+    int completed=0,current_time=0;
+    while (completed < n) {
+        if (q.empty()) {
+            int min_time = INT_MAX, index = -1;
+            for (int i = 0; i < n; ++i) {
+                if (!visited[i] && At[i] < min_time) {
+                    min_time = At[i];
+                    index = i;
+                }
+            }
+            current_time = At[index];
+            q.push(index);
+            visited[index] = true;
+        }
+
         int i = q.front();
         q.pop();
-        if (remaining_bt[i] > tq) {
-            time += tq;
-            remaining_bt[i] -= tq;
+        if (rb[i] > qt) {
+            current_time += qt;
+            rb[i] -= qt;
+            gp.push_back(p[i]);
+            gc.push_back(current_time);
+
+        } else {
+            current_time += rb[i];
+            ct[i] = current_time;
+            rb[i] = 0;
+            completed++;
+            gp.push_back(p[i]);
+            gc.push_back(current_time);
+        }
+        vector<int>a;
+        vector<int>b;
+        for (int j = 0; j < n; ++j) {
+            if (!visited[j] && At[j] <= current_time) {
+                a.push_back(At[j]);
+                b.push_back(j);
+                visited[j] = true;
+            }
+        }
+        for(int k=0;k<a.size();k++){
+            for(int j=k+1;j<a.size();j++){
+                if(a[j]<a[k]){
+                    swap(a[j],a[k]);
+                    swap(b[j],b[k]);
+                }
+            }
+        }
+        for(int j=0;j<b.size();j++){
+            q.push(b[j]);
+        }
+        if (rb[i] > 0) {
             q.push(i);
-        } else {
-            time += remaining_bt[i];
-            ct[i] = time;
-            tat[i] = ct[i];
-            wt[i] = tat[i] - bt[i];
-            remaining_bt[i] = 0;
         }
+    }
+    for (int i = 0; i < n; ++i) {
+        tat[i] = ct[i] - At[i];
+        wt[i] = tat[i] - bt[i];
+    }
+    cout<<"|";
+    for(int i=0;i<gp.size();i++){
+        cout<<"  p"<<gp[i]<<"  |";
+    }
+    cout<<endl;
+    cout<<0;
+    for(int i=0;i<gc.size();i++){
+        cout<<"      "<<gc[i];
+    }
+    cout<<endl;
 
-        gantt.push_back(pid[i]);
+    cout << "\nProcess\tAT\tBT\tCT\tTAT\tWT\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "P" << i+1 << "\t" << At[i] << "\t" << bt[i] << "\t"
+             << ct[i] << "\t" << tat[i] << "\t" << wt[i] << "\n";
     }
-    float total_wt = 0, total_tat = 0;
-    cout << "\nPR\tBT\tCT\tWT\tTAT\n";
-    for (int i = 0; i < n; i++) {
-        total_wt += wt[i];
-        total_tat += tat[i];
-        cout << "P" << pid[i] << "\t" << bt[i] << "\t" << ct[i] << "\t"
-             << wt[i] << "\t" << tat[i] << "\n";
-    }
-
-    cout << fixed << setprecision(2);
-    cout << "\nAverage Waiting Time: " << total_wt / n;
-    cout << "\nAverage Turnaround Time: " << total_tat / n;
-    cout << "\n\nGantt Chart (Order of Execution):\n|";
-    for (int p : gantt) {
-        cout << " P" << p << " |";
-    }
-    cout << "\n";
-    time = 0;
-    for (int p : gantt) {
-        int idx = p - 1;
-        if (bt[idx] > tq) {
-            if (remaining_bt[idx] > 0)
-                bt[idx] -= tq;
-            else
-                bt[idx] -= bt[idx];
-            time += (bt[idx] >= tq ? tq : bt[idx]);
-        } else {
-            time += (bt[idx] >= tq ? tq : bt[idx]);
-        }
-        cout << setw(5) << time;
-    }
-    cout << "\n";
 
     return 0;
 }
